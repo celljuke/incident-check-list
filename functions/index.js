f9dd1f01bac39f19793b3d21e43e787862fb3ca1
celljuke <selcuk@opsgenie.com>
2018-11-15 13:59:10 +0300

    add text

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,5 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  response.send(`<div style="background-color: #ffa100; color:#fff">Hello From Opsgenie ${request.body.text}</div>`);
 });
